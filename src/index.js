b5aceb9093cc21528edffa5ac16a5e2f3abbb06b
Vladislav0101 <by.vlad01@mail.ru>
2019-10-09 00:28:56 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let sum = 0;
+    const numbArr = expr.split(" + ");
+    for(let i = 0; i < numbArr.length; i++){
+        let numSum = Number(numbArr[i]);
+        sum += numSum;
+    }
+    // const number = numbArr.map(noStr => +noStr);
+    // const initialValue = 0.0;
+    // let result = numbArr.reduce((sum, num) => sum + num, initialValue);
+    return sum;
 }
 
 module.exports = {
