2cfe1a16258bc5f14a36f8a6038f350efd5b625d
Vladislav0101 <by.vlad01@mail.ru>
2019-10-12 16:03:20 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,18 +4,39 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let sum = 0;
-    const numbArr = expr.split(" + ");
-    for(let i = 0; i < numbArr.length; i++){
-        let numSum = Number(numbArr[i]);
-        sum += numSum;
-    }
-    // const number = numbArr.map(noStr => +noStr);
-    // const initialValue = 0.0;
-    // let result = numbArr.reduce((sum, num) => sum + num, initialValue);
-    return sum;
+    
+    const parseMultiply = (expr) => {
+        const numbArr = expr.split(' * ');
+        const number = numbArr.map(noStr => +noStr);
+        const initialValue = 1.0;
+        const result = number.reduce((acc, no) => acc * no, initialValue);
+        return result;
+    };
+  
+    const parsePlus = (expr) => {
+      const numbArr = expr.split(' + ');
+      const number = numbArr.map(noStr => parseMultiply(noStr));
+      const initialValue = 0.0;
+      const result = number.reduce((acc, no) => acc + no, initialValue);
+      return result;
+    };
+    const parseMinus = (expr) => {
+        const numbArr = expr.split(' - ');
+        const number = numbArr.map(noStr => parsePlus(noStr));
+        const initialValue = number[0];
+        const result = number.slice(1).reduce((acc, no) => acc - no, initialValue);
+        return result;
+    };
+
+    return (parseMinus(expr));
 }
 
+
+// let sum = 0;
+    // for(let i = 0; i < numbArr.length; i++){
+    //     let numSum = Number(numbArr[i]);
+    //     sum += numSum;
+    // }
 module.exports = {
     expressionCalculator
 }
